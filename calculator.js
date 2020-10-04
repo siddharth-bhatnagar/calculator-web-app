e37fc5cbb1251a49f42112702c2b4431366a978b
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2020-10-04 17:09:33 +0530

    Added bmi calc

--- calculator.js
+++ calculator.js
@@ -18,6 +18,19 @@ app.post("/", (req, res) => {
     res.send("The result of the calculation is " + result);
 });
 
+app.get("/bmicalculator", (req, res) => {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator" , (req, res) => {
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height)/100;
+
+    var BMI = weight/(height * height);
+
+    res.send("Your BMI is " + BMI.toFixed(2));
+
+});
 
 
 app.listen(3000, () => console.log("Server started on port 3000"));
